819d9a6931777a662ea6dd712f1bddcade1ee839
Michael G. Morey <mgmorey@gmail.com>
2019-09-26 15:20:35 -0700

    Add new Dockerfile, SQL scripts and Shell scripts.

---
+++ scripts/run.sh
@@ -0,0 +1,2 @@
+docker run -d -p 3306:3306 --name my-mysql \
+-e MYSQL_ROOT_PASSWORD=supersecret my-mysql
---
+++ sql-scripts/create-tables.sql
@@ -0,0 +1,6 @@
+CREATE TABLE employees (
+    first_name  VARCHAR(25)     NULL,
+    last_name   VARCHAR(25)     NULL,
+    department  VARCHAR(15)     NULL,
+    email       VARCHAR(50)     NULL
+);
